171a323d349cb4e02368d7614669d0edd1410c0c
Lussot Pierre <pierrelidem@gmail.com>
2022-07-01 14:47:11 +0200

    first commit

---
+++ s.js
@@ -0,0 +1,28 @@
+const text = document.querySelector('h1');
+
+
+
+function getChrono(params) {
+    
+const now = new Date().getTime();
+const contdouwnDate = new Date('August 25,2022').getTime();
+
+const distanceBase = contdouwnDate - now;
+//converti en jour
+const days = Math.floor(distanceBase / (1000 * 60 * 60 * 24));
+//converti en heure
+const hour =  Math.floor((distanceBase % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const min =  Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60));
+const sec =  Math.floor((distanceBase % (1000 * 60)) / (1000));
+
+text.innerHTML = days +' J' + '<br> ' + hour + ' H'+'<br>  ' +  min + ' M' + ' <br> ' + sec + ' S'
+}
+
+
+const contDownInterval = setInterval(()=>{
+getChrono()
+
+}, 1000);
+
+getChrono()
+
